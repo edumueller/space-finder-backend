4fb260696a014f3d8f22b91b78e7bd1a0c0f1ee6
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-10-28 18:01:14 -0300

    add api gateway

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -6,7 +6,9 @@ import {
   Runtime,
 } from "aws-cdk-lib/lib/aws-lambda";
 import { join } from "path";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/lib/aws-apigateway";
 export class SpaceStack extends Stack {
+  private api = new RestApi(this, "SpaceApi");
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
@@ -15,5 +17,10 @@ export class SpaceStack extends Stack {
       code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
       handler: "hello.main",
     });
+
+    // Hello Api Lambda Integration
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource("hello");
+    helloLambdaResource.addMethod("GET", helloLambdaIntegration);
   }
 }
